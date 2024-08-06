f2a5e3c44e78c6d9a5dd17c7b4dec5793b88dbd6
hehuiDemon <49153986+hehuiDemon@users.noreply.github.com>
2024-08-06 17:21:29 +0800

    Update test.py

--- plugins/inventory/test.py
+++ plugins/inventory/test.py
@@ -1,11 +1,16 @@
-#/usr/bin/env python3
-import json
+class InventoryModule(BaseInventoryPlugin):
+    NAME = 'test'
 
-data = {
-        "opt": {
-            "hosts": [
-                "192.168.3.61"
-            ]
-        }
-}
-print(json.dumps(data))
+    def verify_file(self, path):
+        valid = False
+        if super(InventoryModule, self).verify_file(path):
+            if path.endswith('yaml') or path.endswith('yml'):
+                valid = True
+        return valid
+
+    def parse(self, inventory, loader, path, cache=True):
+       super(InventoryModule, self).parse(inventory, loader, path, cache)
+       '''Return dynamic inventory from source '''
+       self._read_config_data(path)
+       self.inventory.add_group("test_hosts")
+       self.inventory.add_host(host="192.168.3.61", group="test_hosts")
